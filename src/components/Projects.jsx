ecbee3324e33c6634db75164b7bc1949dd9bc90b
Joe O <j0liverko@gmail.com>
2020-02-26 09:49:33 -0600

    changed project card styling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,36 +5,43 @@ const Project = () => {
     return(
         <section className='projects'>
             <div>
-
-                <img />
+                <h1>Project wahterver</h1>
+                <div className='imgContainer'>
+                    <img />
+                </div>
+                
                 <p>
                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem, accusantium ratione! Reprehenderit, ipsum in. Doloremque, illum nisi corporis possimus blanditiis, soluta enim quaerat, laboriosam consectetur adipisci placeat perferendis! Possimus, sequi. 
                 </p> 
-                <div>
+                <div className='buttonContainer'>
                     <button>View Live</button>
                     <button>View Code</button>    
                 </div>
             </div>
 
             <div>
-
-                <img />
+                <h1>Project wahterver</h1>
+                <div className='imgContainer'>
+                    <img />
+                </div>
                 <p>
                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem, accusantium ratione! Reprehenderit, ipsum in. Doloremque, illum nisi corporis possimus blanditiis, soluta enim quaerat, laboriosam consectetur adipisci placeat perferendis! Possimus, sequi. 
                 </p> 
-                <div>
+                <div className='buttonContainer'>
                     <button>View Live</button>
                     <button>View Code</button>    
                 </div>
             </div>
 
             <div>
-
-                <img />
+                <h1>Project wahterver</h1>
+                <div className='imgContainer'>
+                    <img />
+                </div>
                 <p>
                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem, accusantium ratione! Reprehenderit, ipsum in. Doloremque, illum nisi corporis possimus blanditiis, soluta enim quaerat, laboriosam consectetur adipisci placeat perferendis! Possimus, sequi. 
                 </p> 
-                <div>
+                <div className='buttonContainer'>
                     <button>View Live</button>
                     <button>View Code</button>    
                 </div>
