757478ed4e78e2138153821e8cbef68c8b36056c
Joe O <j0liverko@gmail.com>
2020-02-27 15:49:27 -0600

    removed one project card and fixed nav bug

--- src/components/ParticlesBackground.jsx
+++ src/components/ParticlesBackground.jsx
@@ -20,8 +20,8 @@ const ParticlesBackground = () => {
             		}
             	}}
               style={{
-				width: '100%',
-				height: '100%',
+				width: '100vw',
+				height: '100vh',
 				backgroundColor: 'black',
 				color: 'black',
 				zIndex: '-100',
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,20 +4,7 @@ const Project = () => {
 
     return(
         <section className='projects'>
-            <div>
-                <h1>Project wahterver</h1>
-                <div className='imgContainer'>
-                    <img />
-                </div>
-                
-                <p>
-                   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem, accusantium ratione! Reprehenderit, ipsum in. Doloremque, illum nisi corporis possimus blanditiis, soluta enim quaerat, laboriosam consectetur adipisci placeat perferendis! Possimus, sequi. 
-                </p> 
-                <div className='buttonContainer'>
-                    <button>View Live</button>
-                    <button>View Code</button>    
-                </div>
-            </div>
+            
 
             <div>
                 <h1>Project wahterver</h1>
