cff80ee53fb389faaf9b6315b2b8f1f17404f0ad
Joe O <j0liverko@gmail.com>
2020-04-07 13:29:36 -0500

    added project pictures

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import park from '../imgs/park.png'
+import hub from '../imgs/quality.png'
 const Project = () => {
 
     return(
@@ -9,7 +10,7 @@ const Project = () => {
             <div>
                 <h1>QualityHub</h1>
                 <div className='imgContainer'>
-                    <img />
+                    <img src={hub}/>
                 </div>
                 <p>
                 QualityHub is a web-primary app for both job seekers to polish their skills and for coaches to sell their expertise.
@@ -24,7 +25,7 @@ const Project = () => {
             <div>
                 <h1>Park Passport</h1>
                 <div className='imgContainer'>
-                    <img />
+                    <img src={park}/>
                 </div>
                 <p>
                     Built for a 4 day challenge and designed for park lovers and travellers, Park Passport allows users to search for parks and create new posts about Parks not listed.
